07c6d9c4458b2dfa7cc8dba04e89637f1430a9da
apple471296 <71689201+apple471296@users.noreply.github.com>
2020-09-22 15:47:10 +0800

    Update pxt.json, main.blocks, main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def on_forever():
+    basic.show_number(0)
+    basic.pause(1000)
+    basic.show_number(1)
+    basic.pause(1000)
+    basic.show_number(2)
+    basic.pause(1000)
+    basic.show_number(3)
+    basic.pause(1000)
+    basic.show_number(4)
+    basic.pause(1000)
+    basic.show_number(5)
+basic.forever(on_forever)
